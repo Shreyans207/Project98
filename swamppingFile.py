d96795637ce67b23be836a03b5105391a3e21a70
Shreyans207 <75474973+Shreyans207@users.noreply.github.com>
2021-08-19 13:13:26 +0530

    Update swamppingFile.py

--- swamppingFile.py
+++ swamppingFile.py
@@ -1,5 +1,6 @@
 def SwapFileData() : 
     swapFileName = input('Enter The File Name - ')
+    swapFileName2 = input('Enter The Second File Name Which You Want To Swap With - ')
 
     openedFile = open('sample1.txt')
     data_a = openedFile.read()
@@ -10,7 +11,8 @@ def SwapFileData() :
     openedFile3 = open(swapFileName , 'w')
     openedFile3.write(data_b)
 
-    openedFile4 = open(swapFileName , 'w')
+    openedFile4 = open(swapFileName2 , 'w')
     openedFile4.write(data_a)
+    print('Challange Completed!!')
 
 SwapFileData()
